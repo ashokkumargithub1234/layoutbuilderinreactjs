59cb28c3d5faf978dc2c6cb1e3d087fbf4669081
Ashokkumar <ashokkumarpenjuvvi@gmail.com>
2023-05-08 12:28:06 +0000

    layout builder

---
+++ src/components/ConfigurationController/index.js
@@ -0,0 +1,70 @@
+// Write your code here
+import './index.css'
+import ConfigurationContext from '../../context/ConfigurationContext'
+
+const ConfigurationController = () => (
+  <ConfigurationContext.Consumer>
+    {value => {
+      const {
+        showContent,
+        showLeftNavbar,
+        showRightNavbar,
+        onToggleShowContent,
+        onToggleShowLeftNavbar,
+        onToggleShowRightNavbar,
+      } = value
+      const onChangeContent = event => {
+        onToggleShowContent(event.target.value)
+      }
+      const onChangeLeftNavbar = event => {
+        onToggleShowLeftNavbar(event.target.value)
+      }
+      const onChangeRightNavbar = event => {
+        onToggleShowRightNavbar(event.target.value)
+      }
+      return (
+        <div className="configuration-controls-container">
+          <div className="responsive-controls-container">
+            <h1 className="layout-heading">Layout</h1>
+            <div className="checkbox-group">
+              <div className="checkbox-container">
+                <input
+                  type="checkbox"
+                  onChange={onChangeContent}
+                  id="content"
+                  checked={showContent}
+                />
+                <label htmlFor="content" className="label-text">
+                  Content
+                </label>
+              </div>
+              <div className="checkbox-container">
+                <input
+                  type="checkbox"
+                  onChange={onChangeLeftNavbar}
+                  id="leftNavbar"
+                  checked={showLeftNavbar}
+                />
+                <label htmlFor="leftNavbar" className="label-text">
+                  Left Navbar
+                </label>
+              </div>
+              <div className="checkbox-container">
+                <input
+                  type="checkbox"
+                  onChange={onChangeRightNavbar}
+                  id="rightNavbar"
+                  checked={showRightNavbar}
+                />
+                <label htmlFor="rightNavbar" className="label-text">
+                  Right Navbar
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }}
+  </ConfigurationContext.Consumer>
+)
+export default ConfigurationController
